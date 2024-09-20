fd553c9be2d1733aa10c2aa55601cccd4b944bab
haiduy <haiduy2k3@gmail.com>
2024-09-20 18:29:39 +0700

    add vertical print

--- lib/dart_oop_p2/bt_mang/practise10.dart
+++ lib/dart_oop_p2/bt_mang/practise10.dart
@@ -3,25 +3,36 @@ import 'dart:developer';
 void main() {
   List<int> sums = [];
   var lable = StringBuffer();
+  var result = StringBuffer();
+  var result1 = StringBuffer();
+
   for (int i = 0; i < 9; i++) {
     lable.write('--- $i ---     ');
   }
   log('$lable');
+
   for (int i = 1; i < 10; i++) {
-    String a = '';
+    var hori = StringBuffer();
     int sum = 0;
     for (int j = 1; j < 10; j++) {
+      // horizontal
       if ((i * j) >= 10) {
-        a += '$j x $i = ${j * i}    ';
+        hori.write('$j x $i = ${j * i}    ');
       } else {
-        a += '$j x $i = ${j * i}     ';
+        hori.write('$j x $i = ${j * i}     ');
       }
       sum += (j * i);
+      // vertical
+      if (j == 9) {
+        result1.write('$i x $j = ${i * j}\n\n\n');
+        break;
+      }
+      result1.write('$i x $j = ${i * j}\n');
     }
-    log(a);
+    log('$hori');
     sums.add(sum);
   }
-  var result = StringBuffer();
+
   for (int i = 0; i < sums.length; i++) {
     if (i == 0) {
       result.write(' ----- ${sums[i]}      ');
@@ -29,15 +40,7 @@ void main() {
       result.write(' ----- ${sums[i]}    ');
     }
   }
-  log('$result');
-}
 
-  // a.write(' ----- ${sums[0]}      ');
-  // a.write(' ----- ${sums[1]}    ');
-  // a.write(' ----- ${sums[2]}    ');
-  // a.write(' ----- ${sums[3]}    ');
-  // a.write(' ----- ${sums[4]}    ');
-  // a.write(' ----- ${sums[5]}    ');
-  // a.write(' ----- ${sums[6]}    ');
-  // a.write(' ----- ${sums[7]}    ');
-  // a.write(' ----- ${sums[8]}    ');
+  log('$result');
+  log('$result1');
+}
